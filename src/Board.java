abc209885caa26f5e83267007557885f8a7886da
SandraLeRoi <oawx72@gmail.com>
2021-03-17 17:01:16 +0100

    toujours en travail sur le plateau

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Board {
     protected int[][] echiquier;
+    protected int x,y;
 
-    public void Board() {
+    public Board() {
+        // J'aimerais faire un tableau ou A1 = 11, A2=22 ....
+        //ou x va de 1 à 8 et y de 1 à 8
+        echiquier = new int[8][8];
+        for (int i = 1; i < echiquier.length; i++) {
+            for (int j = 1; j < echiquier[i].length; j++) {
+                x= i;
+                y = j;
+                echiquier[i][j] = x+y;
+            }
+        }
+        System.out.println(echiquier[x][y] + " ");
+    }
 
+    @Override
+    public String toString() {
+        return Arrays.toString(echiquier);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Pawn blackPawn = new Pawn (Color.BLACK, 'A', 1);
         System.out.println(blackPawn);
+
+        Board board = new Board();
+        System.out.println(board);
     }
 }
